e9ce3a73e530106944bd6054d150d4c4a68860d5
COLISEO\ppioli <ppioli@coliseosoftware.com>
2019-02-16 01:16:39 -0300

    Fixed problem in build script

--- Build.ps1
+++ Build.ps1
@@ -1,29 +1,3 @@
-<#
-.SYNOPSIS
-    You can add this to you build script to ensure that psbuild is available before calling
-    Invoke-MSBuild. If psbuild is not available locally it will be downloaded automatically.
-#>
-function EnsurePsbuildInstalled{
-    [cmdletbinding()]
-    param(
-        [string]$psbuildInstallUri = 'https://raw.githubusercontent.com/ligershark/psbuild/master/src/GetPSBuild.ps1'
-    )
-    process{
-        if(-not (Get-Command "Invoke-MsBuild" -errorAction SilentlyContinue)){
-            'Installing psbuild from [{0}]' -f $psbuildInstallUri | Write-Verbose
-            (new-object Net.WebClient).DownloadString($psbuildInstallUri) | iex
-        }
-        else{
-            'psbuild already loaded, skipping download' | Write-Verbose
-        }
-
-        # make sure it's loaded and throw if not
-        if(-not (Get-Command "Invoke-MsBuild" -errorAction SilentlyContinue)){
-            throw ('Unable to install/load psbuild from [{0}]' -f $psbuildInstallUri)
-        }
-    }
-}
-
 # Taken from psake https://github.com/psake/psake
 
 <#
@@ -50,11 +24,8 @@ function Exec
 
 if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
 
-EnsurePsbuildInstalled
-
-exec { & dotnet restore }
-
-Invoke-MSBuild
+exec { & dotnet restore .\src\LetsEncrypt\LetsEncrypt.csproj }
+exec { & dotnet build .\src\LetsEncrypt\LetsEncrypt.csproj -c Release }
 
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
