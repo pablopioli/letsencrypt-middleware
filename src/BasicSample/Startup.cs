5607d89b59ff098ba5ab6eaeb97f9169d04c1f0d
COLISEO\ppioli <ppioli@coliseosoftware.com>
2019-06-06 20:35:54 -0300

    Downgrade dependencies to Net Core 2.1 since is the LTS version

--- src/BasicSample/Startup.cs
+++ src/BasicSample/Startup.cs
@@ -51,7 +51,7 @@ namespace BasicSample
 
             services.AddLetsEncrypt(leOptions);
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
